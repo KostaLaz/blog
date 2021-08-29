e56715f0e0a6ec4196440f2cf090637e873b88ae
KostaLaz <kosta.lazarevski89@gmail.com>
2021-08-29 20:00:38 +0200

    component change

--- src/containers/About/index.js
+++ src/containers/About/index.js
@@ -1,9 +1,11 @@
 import React from 'react'
 
-export const index = () => {
+export const About = (props) => {
     return (
         <div>
             <p>About Page</p>
         </div>
     )
 }
+
+export default About;
