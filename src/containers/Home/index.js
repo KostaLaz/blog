8db3c3a05c703c2e89a9cc75ef059a342efd777b
KostaLaz <kosta.lazarevski89@gmail.com>
2021-08-29 20:00:26 +0200

    component change

--- src/containers/Home/index.js
+++ src/containers/Home/index.js
@@ -4,7 +4,6 @@ import Header from '../../components/Header'
 export const Home = (props) => {
     return (
         <div>
-            <p>Home Page </p>
             <Header />
         </div>
     )
