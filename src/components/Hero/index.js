01a4d8c679412808685d751bba5820a785853883
KostaLaz <kosta.lazarevski89@gmail.com>
2021-09-17 22:54:10 +0200

    Card component added to Hero component

--- src/components/Hero/index.js
+++ src/components/Hero/index.js
@@ -1,11 +1,14 @@
 import React from 'react'
+import Card from '../UI/Card';
 import './style.css';
 
 
-export const Hero = () => {
+export const Hero = (props) => {
     return (
         <div>
-            
+            <Card>
+                Kosta Lazarevski
+            </Card>
         </div>
     )
 }
