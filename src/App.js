71cda057597dde361a07bb3cff2b8c7d2fce40be
KostaLaz <kosta.lazarevski89@gmail.com>
2021-11-07 22:04:23 +0100

    Routing added

--- src/App.js
+++ src/App.js
@@ -2,14 +2,25 @@ import './App.css';
 import Home from './containers/Home';
 import Header from './components/Header';
 import Hero from './components/Hero';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import ContactUs from './containers/ContactUs';
+import Post from './containers/Post';
+import About from './containers/About';
 
 function App() {
   return (
+    <Router>
+
     <div className="App">
        <Header />
        <Hero />
-      <Home />
+      <Route path="/" exact component={Home} />
+      <Route path="/contact-us" exact component={ContactUs} />
+      <Route path="/about-us" exact component={About} />
+      <Route path='/post' exact component={Post} />
     </div>
+  
+    </Router>
   );
 }
 
--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -1,6 +1,7 @@
 import React, { useState } from 'react'
 import './style.css'
-import { FaSistrix } from 'react-icons/fa';  
+import { FaSistrix } from 'react-icons/fa';
+import { NavLink } from 'react-router-dom';
 
 const Navbar = (props) => {
 
@@ -21,11 +22,10 @@ const Navbar = (props) => {
     return (
         <div className="navbar">
             <ul className="navbarMenu">
-                <li><a href="#"> Home</a></li>
-                <li><a href="#"> About Us</a></li>
-                <li><a href="#"> Posts</a></li>
-                <li><a href="#"> Contact Us</a></li>
-                <li><a href="#"> Home</a></li>
+                <li><NavLink to="/"> Home</NavLink></li>
+                <li><NavLink to="about-us"> About Us</NavLink></li>
+                <li><NavLink to="/post"> Posts</NavLink></li>
+                <li><NavLink to="/contact-us"> Contact Us</NavLink></li>
             </ul>
             <form onSubmit={submitSearch}>
                 <div className="search">
