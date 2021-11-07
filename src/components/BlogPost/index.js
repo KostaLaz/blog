007acc52a0819f76957dc3fbcd51542d04d489e5
KostaLaz <kosta.lazarevski89@gmail.com>
2021-11-07 22:34:25 +0100

    Blog Post comonent added

---
+++ src/components/BlogPost/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './style.css';
+
+ const BlogPost = (props) => {
+    return (
+        <div className="blogPost">
+            BlogPost
+        </div>
+    )
+}
+
+export default BlogPost;
