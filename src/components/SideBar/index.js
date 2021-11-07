9a6182828873b5a5b148a899c57324c86d8ccb38
KostaLaz <kosta.lazarevski89@gmail.com>
2021-11-07 22:37:01 +0100

    SideBar component added

---
+++ src/components/SideBar/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './style.css';
+
+const SideBar = (props) => {
+    return (
+        <div className="sideBar">
+            SideBar
+        </div>
+    )
+}
+
+export default SideBar;
