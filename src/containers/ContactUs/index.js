cb18b281ec653e06ab03955835f869ae9be7ed43
KostaLaz <kosta.lazarevski89@gmail.com>
2021-11-11 22:30:53 +0100

    test data added

--- src/containers/ContactUs/index.js
+++ src/containers/ContactUs/index.js
@@ -3,7 +3,7 @@ import React from 'react'
 export const ContactUs = (props) => {
     return (
         <div>
-            <p>Contact Us Page</p>
+            <p>Contact Us</p>
         </div>
     )
 }
